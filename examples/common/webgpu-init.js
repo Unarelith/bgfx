a5c1d8c05d74eb42b6d75e0e133ad4725e84073d
Quentin Bazin <quent42340@gmail.com>
2024-07-15 16:00:58 +0200

    [webgpu-init.js] Increase buffer limits for 41-tess.

--- examples/common/webgpu-init.js
+++ examples/common/webgpu-init.js
@@ -4,8 +4,10 @@ navigator.gpu.requestAdapter().then((adapter) => {
             "float32-filterable"
         ],
         requiredLimits: {
+            maxBufferSize: 536870912, // 512 MiB
             maxComputeInvocationsPerWorkgroup: 512,
-            maxComputeWorkgroupSizeX: 512
+            maxComputeWorkgroupSizeX: 512,
+            maxStorageBufferBindingSize: 536870912
         },
     }).then((device) => {
         Module.preinitializedWebGPUDevice = device;
