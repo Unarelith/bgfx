d16514dbba07aed9b5d55ab0af84873b08bb831a
Quentin Bazin <quent42340@gmail.com>
2024-07-15 15:54:17 +0200

    [webgpu-init.js] Increase workgroup limits for 24-nbody.

--- examples/common/webgpu-init.js
+++ examples/common/webgpu-init.js
@@ -1,6 +1,12 @@
 navigator.gpu.requestAdapter().then((adapter) => {
     adapter.requestDevice({
-        requiredFeatures: ["float32-filterable"]
+        requiredFeatures: [
+            "float32-filterable"
+        ],
+        requiredLimits: {
+            maxComputeInvocationsPerWorkgroup: 512,
+            maxComputeWorkgroupSizeX: 512
+        },
     }).then((device) => {
         Module.preinitializedWebGPUDevice = device;
     });
