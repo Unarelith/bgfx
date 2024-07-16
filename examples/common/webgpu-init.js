12591566fe5125860fd374b4729eb654fc786186
Quentin Bazin <quent42340@gmail.com>
2024-07-17 00:12:44 +0200

    [webgpu-init.js] Increased limit for 39-assao.

--- examples/common/webgpu-init.js
+++ examples/common/webgpu-init.js
@@ -7,7 +7,8 @@ navigator.gpu.requestAdapter().then((adapter) => {
             maxBufferSize: 536870912, // 512 MiB
             maxComputeInvocationsPerWorkgroup: 512,
             maxComputeWorkgroupSizeX: 512,
-            maxStorageBufferBindingSize: 536870912
+            maxStorageBufferBindingSize: 536870912,
+            maxStorageTexturesPerShaderStage: 5
         },
     }).then((device) => {
         Module.preinitializedWebGPUDevice = device;
